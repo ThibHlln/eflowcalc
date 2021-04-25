3aa7c93c272d399e9d876438eb86137e2bcbd84b
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-04-25 17:00:03 +0200

    add example in calculator docstring and add doctest on it

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,9 +8,6 @@ if __name__ == '__main__':
     test_loader = unittest.TestLoader()
     test_suite = unittest.TestSuite()
 
-    all_tests = test_loader.discover('.', pattern='test_*.py')
-    test_suite.addTests(all_tests)
-
     test_suite.addTests(doctest.DocTestSuite(eflowcalc.eflowcalc))
 
     runner = unittest.TextTestRunner(verbosity=2)
